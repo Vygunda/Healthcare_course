9bced7dfa17f9127557aadf91e6d7e619b302354
Vygunda <157414394+Vygunda@users.noreply.github.com>
2024-03-26 15:58:08 -0500

    final_script.sh
    Project-1 script

--- final_script.sh
+++ final_script.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
-# I did this process for 12 bed files by changing the file names manually
-#ENCFF027BPY.bed  ENCFF041GGN.bed  ENCFF168GAN.bed  ENCFF362RCY.bed  ENCFF534UAS.bed  ENCFF886GET.bed  
-#ENCFF027ZYL.bed  ENCFF052DSF.bed  ENCFF260PVK.bed  ENCFF448LQD.bed  ENCFF829XGX.bed  ENCFF931FYG.bed  
+# I did this process for 10 bed files by changing the file names manually
+#Below are the files that I used
+#ENCFF168GAN.bed  ENCFF052DSF.bed  ENCFF253LML.bed  ENCFF849QFO.bed  ENCFF041GGN.bed  ENCFF922ABK/.bed  
+#ENCFF180WXO.bed  ENCFF386GNZ.bed  ENCFF975INV.bed  ENCFF928NLF.bed   
 
-# first I have converted all the above mentioned bed files into .txt files using bed tools 
-#bedtools getfasta -fi /users/Vygunda/reference/hg38.fa -bed "ENCFF027BPY.bed" -fo "ENCFF027BPY.txt"
+#Downloaded the above 10 different files bed narrowPeak file type from 10 different experiments.
 
-# Step 1: Process ENCFF041GGN.txt
+# get the required bed file using wget command before executing this script : Download the data from the encode website
+# wget https://www.encodeproject.org/files/ENCFF041GGN/@@download/ENCFF041GGN.bed.gz
+
+# Convert the narrowPeakfile into a nucleotide(ATGCs)file using bedtools
+bedtools getfasta -fi /users/Vygunda/reference/hg38.fa -bed "ENCFF041GGN.bed" -fo "ENCFF041GGN.txt"
+
+# Step 1: Extract all the nucleotides from this file 
 input_file="ENCFF041GGN.txt"
 grepped_file="grepped_accessible_regions.txt"
-output_file="ENCFF041GGN_output_trimmed_accessible_regions.txt"
+output_file="ENCFF041GGN_output_trimmed_accessible_regions.txt"  # This is the accessible file
 
 # Perform grep to filter lines not containing ">"
 grep -v ">" "$input_file" > "$grepped_file"
+
+#this is the standard file which we use to get the average length 
+grepped_standard_file="grepped_standard_accessible_regions.txt"
+standard_file="ENCFF041GGN.txt"
+grep -v ">" "$standard_file" > "$grepped_standard_file"
 
 # Calculate average sequence length
 total_length=0
 total_sequences=0
 
-# Loop through each sequence in the file
+# Loop through each sequence in the standard file
 while read -r sequence; do
     length=${#sequence}
     total_length=$((total_length + length))
     total_sequences=$((total_sequences + 1))
-done < "$grepped_file"
+done < "$grepped_standard_file"
 
 average_length=$((total_length / total_sequences))
 
-# Output sequences with the target length
+# the average length obtained is used for all the sequences 
+#if a sequence is greater than or equal to the average_length it will be trimmed to the average length and the rest are discarded
+
+#The gripped file is trimmed as per the above conditions
 while read -r sequence; do
     length=${#sequence}
     if [ "$length" -ge "$average_length" ]; then
@@ -40,9 +54,9 @@ done < "$grepped_file" > "$output_file"
 # Clean up temporary file
 rm "$grepped_file"
 
-# Step 2: Process ENCFF041GGN.bed
+# Step 2: Process ENCFF041GGN.bed to get negative regions
 input_bed="ENCFF041GGN.bed"
-output_negative="ENCFF041GGN_negative_regions.bed"
+output_negative="ENCFF041GGN_negative_regions.bed"  # negative regions file 
 
 # Sort the BED file
 sort -k1,1 -k2,2n "$input_bed" > sorted.bed
@@ -66,10 +80,10 @@ done < sorted.bed
 rm sorted.bed
 
 # Apply awk to format output file
-awk '{$1=$1}1' OFS="\t" "$output_negative" > "ENCFF041GGN_negative_regions_tab.bed"
+awk '{$1=$1}1' OFS="\t" "$output_negative" > "ENCFF041GGN_negative_regions_tab.bed" 
 
 
-
+# run the negative regions bed file to get negative txt file 
 # Step 3: Run bedtools getfasta
 output_negative_tab="ENCFF041GGN_negative_regions_tab.bed"
 output_negative_fasta="ENCFF041GGN_negative_regions_tab.txt"
@@ -85,15 +99,10 @@ output_file="ENCFF041GGN_trimmed_negative_regions.txt"
 total_length=0
 total_sequences=0
 
-# Loop through each sequence in the file
-while read -r sequence; do
-    length=${#sequence}
-    total_length=$((total_length + length))
-    total_sequences=$((total_sequences + 1))
-done < "$input_file"
-
-average_length=$((total_length / total_sequences))
-
+#use the same average_length obtained from the accessible region standard file
+# the average length obtained is used for all the sequences 
+#if a sequence is greater than or equal to the average_length it will be trimmed to the average length and the rest are discarded
+#The gripped file is trimmed as per the above condition
 # Output sequences with the target length
 while read -r sequence; do
     length=${#sequence}
@@ -105,4 +114,3 @@ done < "$input_file" > "$output_file"
 
 # Clean up temporary file
 rm "$input_file"
-
